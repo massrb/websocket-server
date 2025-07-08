1ed04dfadda79626c9205bd371245a9cbd19cde1
laurence <massrubydev@gmail.com>
2025-07-08 15:27:35 -0400

    got web sockets working, and turbo functionality

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -1,4 +1,8 @@
 module ApplicationCable
   class Connection < ActionCable::Connection::Base
+    def connect
+      # puts "⚡ Origin: #{request.origin.inspect}"
+      # Add your connection logic if needed
+    end
   end
 end
--- app/models/message.rb
+++ app/models/message.rb
@@ -1,2 +1,5 @@
 class Message < ApplicationRecord
+  after_create_commit -> {
+    broadcast_append_to "messages", target: "messages", partial: "messages/message"
+  }
 end
