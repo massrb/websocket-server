7f108522a93f4a7a0045bb0715e3945fa06a860d
laurence <massrubydev@gmail.com>
2025-07-18 19:02:37 -0400

    try a different way to fix render.com cable problem

--- app/models/message.rb
+++ app/models/message.rb
@@ -15,14 +15,16 @@ class Message < ApplicationRecord
       puts "message conenctions; Name: #{index.name}, Unique: #{index.unique}, Columns: #{index.columns.inspect}"
     end
 
+    puts 'model primary key:' + Message.primary_key.inspect
+
     primary_key = Message.connection.primary_key('messages')
     Rails.logger.debug "Primary key for messages table: #{primary_key.inspect}"
-    
+
     Rails.logger.debug "=== INDEX DEBUG END ==="
 
     broadcast_append_to "messages", target: "messages", 
                         partial: "messages/message", 
-                        locals: { message: self }, unique_by: :id
+                        locals: { message: self }, unique_by: ->(message) { message.id }
 
 
     total = Message.count
