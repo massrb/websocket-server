bc9f3a47090a797d16cc38f689d05829892113a1
laurence <massrubydev@gmail.com>
2025-07-18 11:35:27 -0400

    delete records needed ids to prevent postgress error

--- app/models/message.rb
+++ app/models/message.rb
@@ -4,20 +4,13 @@ class Message < ApplicationRecord
   private
 
   def broadcast_message_and_trim_old
-    # Broadcast the newly created message
     broadcast_append_to "messages", target: "messages", partial: "messages/message"
 
-    # Trim oldest messages, keeping the latest 200 only
-    trim_old_messages
-  end
-
-  def trim_old_messages
-    # Count messages and only trim if there are too many
-    excess_count = Message.count - 200
-    return if excess_count <= 0
-
-    # Delete the oldest records beyond the latest 200
-    Message.order(created_at: :asc).limit(excess_count).delete_all
+    total = Message.count
+    if total > 200
+      # Get oldest message IDs to delete
+      excess_ids = Message.order(id: :asc).limit(total - 200).pluck(:id)
+      Message.where(id: excess_ids).delete_all
+    end
   end
 end
-
